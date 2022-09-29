bd15d069214d0ec81081f2375be2503e4dccc3b2
2sssg <lsg020300@naver.com>
2022-09-29 17:37:53 +0900

    Feat: Sample Controller

---
+++ src/main/java/com/example/springsecuritypractice/SampleController.java
@@ -0,0 +1,41 @@
+package com.example.springsecuritypractice;
+
+import java.security.Principal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+	@GetMapping("/")
+	public String index(Model model, Principal principal) {
+		if (principal == null) {
+			model.addAttribute("message", "Hello, Spring Security");
+		} else {
+			model.addAttribute("message", "Welcome back, " + principal.getName());
+		}
+
+		return ("index");
+	}
+
+	@GetMapping("/info")
+	public String info(Model model) {
+		model.addAttribute("message", "Here are more information");
+
+		return ("info");
+	}
+
+	@GetMapping("/dashboard")
+	public String dashboard(Model model, Principal principal) {
+		model.addAttribute("message", "Hi, " + principal.getName());
+
+		return ("dashboard");
+	}
+
+	@GetMapping("/admin")
+	public String admin(Model model, Principal principal) {
+		model.addAttribute("message", "Hello Admin, " + principal.getName());
+		return ("admin");
+	}
+}
